e8e7ef5fcd32a9790fdaf531ae1b6465abb9ed02
Leticia Sanches <106995301+Lsats@users.noreply.github.com>
2025-07-27 18:40:41 -0300

    Update SalLiq.c

--- SalLiq.c
+++ SalLiq.c
@@ -1,4 +1,3 @@
-//Faça um algoritmo que efetue o cálculo do salário líquido de um professor. As informações fornecidas serão: valor da hora aula, número de aulas lecionadas no mês e percentual de desconto do INSS. Imprima na tela o salário líquido final.
 
 void SalLiq(float hr, int nr);
 
